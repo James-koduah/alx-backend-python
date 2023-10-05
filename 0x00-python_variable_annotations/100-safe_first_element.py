f0c83a62a9826b3eb1c3a70f267d9d6f015771a2
James Koduah <jameskoduah171@gmail.com>
2023-10-05 11:36:27 +0000

    100-safe_first_element.py: longer comment

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-Duck type annotations
+Duck type annotations ddddddddddddddddddddddddddddddddddddddddddddd
 """
 from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[None, Any]:
-    """return first item of list"""
+    """return first item of list dddddddd"""
     if lst:
         return lst[0]
     else:
