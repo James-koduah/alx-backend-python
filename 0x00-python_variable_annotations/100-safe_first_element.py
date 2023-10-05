5022d7679261dad178b7bbba8fc3860bc1f94fbb
James Koduah <jameskoduah171@gmail.com>
2023-10-05 11:35:09 +0000

    100-safe_first_element.py: forgot to add comment to function

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,6 +6,7 @@ from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[None, Any]:
+    """return first item of list"""
     if lst:
         return lst[0]
     else:
