8f20fef1251ff8bfb252909192772241ec19aa9c
James Koduah <jameskoduah171@gmail.com>
2023-10-05 12:05:43 +0000

    101-safely_get_value.py: added comment to function

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -12,6 +12,7 @@ def safely_get_value(
         dct: Mapping,
         key: Any,
         default: Union[T, None] = None) -> Union[Any, T]:
+    """return something representing something"""
     if key in dct:
         return dct[key]
     else:
