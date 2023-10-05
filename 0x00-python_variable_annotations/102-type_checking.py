1c485780f976219059600843f667b60bf78555d6
James Koduah <jameskoduah171@gmail.com>
2023-10-05 14:28:37 +0000

    102-type_checking.py: first

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Use mypy to validate the following piece of code and apply any necessary change
+"""
+from typing import Tuple, List, Any, Iterable
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    zoomed_in: List[Any] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
