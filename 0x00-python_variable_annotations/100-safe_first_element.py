7b781a2a2f3676ff85ac5d1dd0ab00f7514b6d8a
James Koduah <jameskoduah171@gmail.com>
2023-10-05 11:40:14 +0000

    hh

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -5,6 +5,7 @@ Duck type annotations ddddddddddddddddddddddddddddddddddddddddddddd
 from typing import Sequence, Any, Union
 
 
+# The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[None, Any]:
     """return first item of list dddddddd"""
     if lst:
