8f9bfc37a3371ad72c381a9f540f4ebf037fefad
James Koduah <jameskoduah171@gmail.com>
2023-10-09 19:33:33 +0000

    4-tasks.py: first

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Take the code from wait_n and alter it into a new function task_wait_n.
+The code is nearly identical to wait_n except task_wait_random is being called
+"""
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """explanation aboue there"""
+    funcs = [task_wait_random(max_delay) for i in range(n)]
+    final = [await func for func in asyncio.as_completed(funcs)]
+    return final
