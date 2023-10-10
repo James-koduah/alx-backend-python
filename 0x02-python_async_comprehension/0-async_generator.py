f135e88f87940f0677989c3c1505c0c4a37078f8
James Koduah <jameskoduah171@gmail.com>
2023-10-10 13:17:10 +0000

    0-async_generator.py: added comment to the function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,6 +8,7 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[int, None, None]:
+    """Generate random numbers and sleep for one second"""
     for i in range(0, 10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
